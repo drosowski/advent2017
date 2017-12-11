40f1781a35b60586359caf8ca4344ede9ff752fa
Daniel Rosowski <rosowski@smartsquare.de>
2017-12-11 10:26:22 +0100

    Day6 fix

--- src/advent2017/day6.clj
+++ src/advent2017/day6.clj
@@ -6,12 +6,15 @@
          step 1]
      (let [mx (apply max block)
            mxidx (.indexOf block mx)
-           incr (int (Math/ceil (/ mx (count block))))
-           nblock (map-indexed (fn [idx elem]
-                      (if (= idx mxidx)
-                      (- elem (* (dec (count block)) incr))
-                      (+ incr elem))) block)]
-        (prn nblock)
+           nblock (loop [nblock (assoc block mxidx 0)
+                         idx mxidx
+                         incr mx]
+                    (let [nidx (if (>= (inc idx) (count nblock))
+                                  0
+                                  (inc idx))]
+                      (if (= incr 0)
+                        nblock
+                        (recur (update-in nblock [nidx] inc) nidx (dec incr)))))]
         (if (> (.indexOf pastblocks nblock) -1)
           step
           (recur nblock (conj pastblocks nblock) (inc step))))))
